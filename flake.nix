5617d0cb86318da50b93cb05897870c9953b249f
Will C IV <cleghornw04@proton.me>
2024-01-12 10:53:34 -0700

    flake init

---
+++ flake.nix
@@ -0,0 +1,42 @@
+{
+  description = "Development shell for Rust development.";
+
+  inputs = {
+    nixpkgs.url      = "github:NixOS/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-utils.url  = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, rust-overlay, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+      in
+      with pkgs;
+      {
+        devShells.default = mkShell {
+          buildInputs = [
+            openssl
+            pkg-config
+            eza
+            fd
+            zellij
+            rustup
+            (rust-bin.selectLatestNightlyWith (toolchain: toolchain.default.override {
+              extensions = [ "rust-src" "rust-analyzer"];
+            }))
+          ];
+
+          shellHook = ''
+            alias ls=eza
+            alias find=fd
+            cp /run/current-system/sw/bin/rust-analyzer ~/.local/share/nvim/mason/bin/
+            alias zj="zellij --layout .layout.kdl"
+          '';
+        };
+      }
+    );
+}
